bb075f07e78aa2fb1261368da977d5985d91bd76
Mohini <mohinikar1497@gmail.com>
2026-03-03 19:30:45 +0530

    hello

--- src/pkg1/democlass.java
+++ src/pkg1/democlass.java
@@ -5,5 +5,6 @@ public class democlass {
 	public static void main(String[] args) {
 		System.out.println("hello demo");
 		System.out.println("return after first commit");
+		System.out.println("hello terminal");
 	}
 }
