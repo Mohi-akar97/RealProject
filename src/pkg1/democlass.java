6955638fe94c3fe913f428e1641a2c0cb02aea18
Mohi-akar97 <148572004+Mohi-akar97@users.noreply.github.com>
2026-03-04 18:47:11 +0530

    Update democlass.java

--- src/pkg1/democlass.java
+++ src/pkg1/democlass.java
@@ -4,6 +4,7 @@ public class democlass {
 
 	public static void main(String[] args) {
 		System.out.println("new project created clone to your system");
+		System.out.println("small updates are available");
 
 
 	}
